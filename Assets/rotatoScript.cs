22ee79cb38fb084a4a7ddc5ad719f1b2a30a22ca
Jack Chu <jack@jackchu.com>
2016-04-17 02:27:38 -0400

    fix rorateScript.cs

--- Assets/rotatoScript.cs
+++ Assets/rotatoScript.cs
@@ -2,15 +2,7 @@
 using System.Collections;
 
 public class rotatoScript : MonoBehaviour {
-
-	public float speed = 5f;
-	public GameObject gemParent;
-	private Vector3 rotationDirection;
-
 	void Update (){
-		 rotationDirection = new Vector3(0, 1, 0);
-		 gemParent = gemParent.transform.parent;
-		 gemParent.transform.Rotate((rotationDirection * Time.deltaTime * speed));
-	
+		transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
 	}
 }
