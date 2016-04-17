fe869bfcc324668ebd7f04a298c1c0ebebd1a1bb
Christina Goefron <cagoefron@gmail.com>
2016-04-17 19:02:42 -0400

    added opening story text and disabled controller for beginning of scene.

---
+++ Assets/openingStoryScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openingStoryScript : MonoBehaviour {
+
+// on start, disable player controls
+//shows canvas and plays voiceover
+// after timer, thisobject disables and player controls fully enabled
+
+	private GameObject playerObject;
+	private float timer;
+	public float timerLength;
+	private bool playerEnabled;
+
+	void Awake() {
+		playerObject = GameObject.FindWithTag("Player");
+	}
+
+	void Update() {
+		Debug.Log ("timer =" + timer);
+		//start timer
+
+		//disable playerObject
+		if (timer < timerLength) {
+			timer += Time.time;
+			playerEnabled = false;
+			playerObject.active = false;
+		} else {
+			playerEnabled = true;
+			playerObject.active = true;
+			gameObject.active = false;
+		}
+		//play voiceover
+	}
+
+}
