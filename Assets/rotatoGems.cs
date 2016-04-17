a9b31c889a1e478b0d324c73a93f80986294c189
Christina Goefron <cagoefron@gmail.com>
2016-04-17 15:33:38 -0400

    Try this again. Rotatogems in my original scene. Fixed rotatoScript, now split with rotatoGems script.

---
+++ Assets/rotatoGems.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatoGems : MonoBehaviour {
+
+	public float speed;
+
+	void Update (){
+		transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+	}
+}
--- Assets/rotatoScript.cs
+++ Assets/rotatoScript.cs
@@ -5,6 +5,7 @@ public class rotatoScript : MonoBehaviour {
 	public float speed = 1.0F;
 	public float scale = 1.0F;
 
+
 	void LateUpdate (){
 		transform.RotateAround(transform.position, Vector3.up, (0.3F + Input.acceleration.x) * speed);
 		foreach (Transform child in transform) {
@@ -13,5 +14,6 @@ public class rotatoScript : MonoBehaviour {
 			float scaleZ = Mathf.Clamp((child.transform.localScale.z + Input.acceleration.z), 10.0F, 300.0F);
 			child.transform.localScale = new Vector3 (scaleX, scaleY, scaleZ) * (1.0F * scale);
 		}
+
 	}
 }
