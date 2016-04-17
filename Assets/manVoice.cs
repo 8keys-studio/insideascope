db256552c7c008e69e9ca2e682b36448b1531e6f
Christina Goefron <cagoefron@gmail.com>
2016-04-17 19:57:43 -0400

    Adding audio players, made manStatue a prefab, fixed text.

---
+++ Assets/manVoice.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manVoice : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			AudioSource.Play();
+		}
+	}
+}
