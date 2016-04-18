e150fbccd08b5dff4956903d02e29c3d8d4d3c31
Chris Bonilla <ninjatech@gmail.com>
2016-04-17 22:55:01 -0400

    Added level transition. Failed to get fade in/out working

---
+++ Assets/EnterLoveTower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterLoveTower : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Trigger Level Change");
+            StartCoroutine(ChangeLevel());
+        }
+    }
+
+    IEnumerator ChangeLevel ()
+    {
+        Debug.Log("People");
+        float fadeTime = GameObject.Find("fadeInOut").GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        Debug.Log("Places");
+        Application.LoadLevel("christinaTestScene"); //make a win message
+        //gem1
+    }
+}
---
+++ Assets/Fading.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fading : MonoBehaviour {
+
+    public Texture2D fadeOutTexture;
+    public float fadeSpeed = 0.8f;
+
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+
+    //private bool sceneStarting = true;
+    private int fadeDir = -1;
+
+    /*
+    void Awake ()
+    {
+        guiTexture. = new Rect(0, 0, Screen.width, Screen.height);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FadeToClear()
+    {
+        guiTexture.color = Color.Lerp(guiTexture.color
+    }
+    */
+
+    void OnGui ()
+    {
+        Debug.Log("Fade: " + fadeSpeed);
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b);
+        GUI.depth = drawDepth;
+        GUI.DrawTexture ( new Rect (0,0, Screen.width, Screen.height), fadeOutTexture);
+    }
+
+    public float BeginFade (int direction)
+    {
+        Debug.Log("Begin Fade: " + fadeDir);
+        fadeDir = direction;
+        return (fadeSpeed);
+    }
+
+    void OnLevelWasLoaded (int level)
+    {
+        Debug.Log("Level Loaded: " + level);
+
+        BeginFade(-1);
+    }
+
+}
