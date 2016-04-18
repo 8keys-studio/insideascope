0c80550a263af341db686be4fa0bb930dab8e627
Christina Goefron <cagoefron@gmail.com>
2016-04-17 21:27:49 -0400

    Added heart gems object with movement script, fixed man voice script

---
+++ Assets/heartGemMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heartGemMovement : MonoBehaviour {
+
+	public float speed;
+	public Transform target;
+	public float floatSpeed;
+
+	void Update (){
+		float step = floatSpeed * Time.deltaTime;
+		transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+}
